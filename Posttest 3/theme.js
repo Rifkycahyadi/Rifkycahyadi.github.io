7cc3f61c68dd15d57e812a5e3fac6c29a22acc6e
Rifkycahyadi <74778010+Rifkycahyadi@users.noreply.github.com>
2022-10-04 23:19:51 +0800

    Posttest 3

---
+++ Posttest 3/theme.js
@@ -0,0 +1,15 @@
+ function myFunction(){
+    var dom = document.body;
+    dom.classList.toggle("Content")
+
+    var element = document.body;
+    element.classList.toggle("Header");
+    element.classList.toggle("footer");
+ }//  button.addEventListener("click", function showInfo() {
+//     const x = document.getElementById('info');
+//     if (x.style.display == 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+//  });
