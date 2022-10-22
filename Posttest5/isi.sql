8f0c3ee49936a0203b2546c0990720fdbfb0bcc9
Rifkycahyadi <74778010+Rifkycahyadi@users.noreply.github.com>
2022-10-22 23:36:01 +0800

    Add files via upload

---
+++ Posttest5/isi.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Okt 2022 pada 17.35
+-- Versi server: 10.4.19-MariaDB
+-- Versi PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kuliner`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `isi`
+--
+
+CREATE TABLE `isi` (
+  `id` int(11) NOT NULL,
+  `masakan` varchar(30) NOT NULL,
+  `daerah` varchar(30) NOT NULL,
+  `resep` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `isi`
+--
+
+INSERT INTO `isi` (`id`, `masakan`, `daerah`, `resep`) VALUES
+(1, 'tambunsu', 'padang', 'resep');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `isi`
+--
+ALTER TABLE `isi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `isi`
+--
+ALTER TABLE `isi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
